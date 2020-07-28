793630bfdad252a15a8d20c79cc291b901f6de59
Jesus Velazquez <jvelaz46@ford.com>
2020-07-28 17:08:08 -0500

    Create Mole component with show property and transition

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app" class="h-screen w-full bg-orange-400 px-4 pt-4 text-yellow-100 shadow-lg md:px-40 md:pt-20">
+  <div id="app" class="h-screen w-full bg-purple-900 px-4 pt-4 text-yellow-100 shadow-lg md:px-48 md:pt-20">
     <div class="flex items-center justify-between">
       <div class="text-sm flex flex-col justify-center">
         <div class="tracking-wider text-center">HIGH SCORE</div>
@@ -11,10 +11,10 @@
       </div>
     </div>
 
-    <h1 class="text-center text-3xl my-3 tracking-wider font-bolder">Whack-a-mole!</h1>
+    <h1 class="text-center text-3xl my-3 tracking-wider font-bolder">Whack-a-<span class="text-green-500">Vue</span>!</h1>
     
-    <div class="md:w:3/5 flex justify-center items-center my-2">
-      <div class="flex justify-between">
+    <div class="md:w:4/5 flex justify-center items-center my-4">
+      <div class="flex justify-between items-center">
         <div class="mx-2"> 
           <div class="w-full text-center">Level</div>
           <div class="score px-2">10</div>
@@ -23,61 +23,37 @@
           <div class="w-full text-center">Time</div>
           <div class="score px-2">0:30</div>
         </div>
-        <button class="rounded-lg px-4 py-2 mx-2 border-2 border-green-400 shadow-lg bg-green-500 hover:bg-green-600" >Start!</button>
+        <button @click="whack" class="rounded-lg px-4 py-2 mx-2 border-2 border-green-300 shadow-lg bg-green-500 hover:bg-green-300 hover:text-green-700" >Start!</button>
       </div>
     </div>
 
 
-    <div class="grid grid-rows-3">
-      <div class="hole hole1">
-        <div class="mole"></div>
-      </div>
-      <div class="hole hole2">
-        <div class="mole"></div>
-      </div>
-      <div class="hole hole3">
-        <div class="mole"></div>
-      </div>
-      <div class="hole hole4">
-        <div class="mole"></div>
-      </div>
-      <div class="hole hole5">
-        <div class="mole"></div>
-      </div>
-      <div class="hole hole6">
-        <div class="mole"></div>
-      </div>
-    </div>
-
-    <div class="game bg-yellow-300">
-      <div class="hole hole1">
-        <div class="mole"></div>
-      </div>
-      <div class="hole hole2">
-        <div class="mole"></div>
-      </div>
-      <div class="hole hole3">
-        <div class="mole"></div>
-      </div>
-      <div class="hole hole4">
-        <div class="mole"></div>
-      </div>
-      <div class="hole hole5">
-        <div class="mole"></div>
-      </div>
-      <div class="hole hole6">
-        <div class="mole"></div>
-      </div>
+    <div class="grid grid-cols-3 md:gap-4 rounded shadow-lg md:4/5 bg-yellow-400 py-2">
+      <Mole :show="mole"/>
     </div>
   </div>
 </template>
 
 <script>
+import Mole from '@/components/Mole'
 
 export default {
   name: 'App',
+
+  data: function () {
+    return {
+      mole: true,
+    }
+  },
   components: {
+    Mole
+  },
+  methods: {
+    whack: function() {
+      return this.mole = !this.mole
+    }
   }
+
 }
 </script>
 
---
+++ src/components/Mole.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="hole h-24 flex justify-center">
+    <transition name="moleMove">
+      <img v-if="show" class="w-8/12 md:w-full lg:w-4/5" src="./../assets/mole.svg"/>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    show: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style>
+.moleMove-enter-active {
+  animation: popOut 0.25s ease-in;
+}
+@keyframes popOut {
+  0%{
+    transform: translateY(400px);
+  }
+  100%{
+    transform: translateY(0px);
+  }
+}
+.moleMove-leave-active {
+  animation: popIn 0.25s ease-in;
+}
+@keyframes popIn {
+  0%{
+    transform: translateY(0px);
+  }
+  100%{
+    transform: translateY(400px);
+  }
+}
+</style>
