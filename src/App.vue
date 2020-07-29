637d1fdaf8a1a90f0a788cfdd7131eaf58c4f12f
Jesus Velazquez <jvelaz46@ford.com>
2020-07-29 10:10:49 -0500

    Add score and high score tracking. Add method to increase score based on level and hide mole when it is clicked

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,11 @@
     <div class="flex items-center justify-between">
       <div class="text-sm flex flex-col justify-center">
         <div class="tracking-wider text-center">HIGH SCORE</div>
-        <div class="score">00000</div>
+        <div class="score">{{displayHighScore}}</div>
       </div>
       <div class="text-sm flex flex-col justify-center tracking-wider">
         <div class="tracking-wider text-center">YOUR SCORE</div>
-        <div class="score">00000</div>
+        <div class="score">{{displayScore}}</div>
       </div>
     </div>
 
@@ -17,19 +17,19 @@
       <div class="flex justify-between items-center">
         <div class="mx-2"> 
           <div class="w-full text-center">Level</div>
-          <div class="score px-2">10</div>
+          <div class="score px-2">{{level}}</div>
         </div>
         <div class="px-4 py-2 mx-4"> 
           <div class="w-full text-center">Time</div>
           <div class="score px-2">0:30</div>
         </div>
-        <button @click="whack" class="rounded-lg px-4 py-2 mx-2 border-2 border-green-300 shadow-lg bg-green-500 hover:bg-green-300 hover:text-green-700" >Start!</button>
+        <button @click="getHole" class="rounded-lg px-4 py-2 mx-2 border-2 border-green-300 shadow-lg bg-green-500 hover:bg-green-300 hover:text-green-700" >Start!</button>
       </div>
     </div>
 
 
     <div class="grid grid-cols-3 md:gap-4 rounded shadow-lg md:4/5 bg-yellow-400 py-2">
-      <Mole :show="mole"/>
+      <Mole v-for="m in moles" v-on:whacked="bonk" :id="m.id" :show="m.show" :key="m.id"/>
     </div>
   </div>
 </template>
@@ -42,15 +42,74 @@ export default {
 
   data: function () {
     return {
-      mole: true,
+      highScore: 0,
+      score: 0,
+      time: 0,
+      level: 1,
+      lastHole: null,
+      moles: [
+        {id:0, show: false},
+        {id:1, show: false},
+        {id:2, show: true},
+        {id:3, show: false},
+        {id:4, show: true},
+        {id:5, show: false},
+      ]
     }
   },
+
+  computed: {
+    displayScore: function () {
+      return this.fillWithZeros(8, this.score)
+    },
+    displayHighScore: function () {
+      return this.fillWithZeros(8, this.highScore)
+    },
+  },
+
+  mounted: function () {
+    // Retrieve high score
+    this.highScore = 0 | JSON.parse(localStorage.getItem('highScore'))
+  },
+
   components: {
     Mole
   },
+
   methods: {
-    whack: function() {
-      return this.mole = !this.mole
+    bonk: function(id) {
+      console.log(id)
+      this.moles[id].show = false
+      this.score += this.level * 10
+      if(this.score > this.highScore) {
+        this.highScore = this.score
+        localStorage.setItem('highScore', this.highScore)
+      }
+    },
+    
+    getTime: function (min, max) {
+      // get a random time
+      return Math.round(Math.random() * (max - min) + min); // map random to [max, min]
+    },
+
+    getHole: function randomHole() {
+      // Get a random hole, don't repeat last one
+      let hole
+      do {
+        const idx = Math.floor( Math.random() * this.moles.length )
+        hole = this.moles[idx];
+      } while (hole === this.lastHole)
+      this.lastHole = hole;
+      console.log(hole.id)
+      return hole.show = true;
+    },
+
+    fillWithZeros: function (max, score) {
+      const ss = score.toString().split('');
+      for (let k=0; k<=max-ss.length; k++) {
+        ss.unshift('0')
+      }
+      return ss.join('')
     }
   }
 
--- src/components/Mole.vue
+++ src/components/Mole.vue
@@ -1,17 +1,26 @@
 <template>
   <div class="hole h-24 flex justify-center">
     <transition name="moleMove">
-      <img v-if="show" class="w-8/12 md:w-full lg:w-4/5" src="./../assets/mole.svg"/>
+      <img v-if="show" @click.prevent="whack" class="w-8/12 md:w-full lg:w-4/5" src="./../assets/mole.svg"/>
     </transition>
   </div>
 </template>
 
 <script>
+
 export default {
   props: {
+    id: {
+      type:Number
+    },
     show: {
       type: Boolean,
       default: false
+    }
+  },
+  methods: {
+    whack: function() {
+      this.$emit('whacked', this.id)
     }
   }
 }
